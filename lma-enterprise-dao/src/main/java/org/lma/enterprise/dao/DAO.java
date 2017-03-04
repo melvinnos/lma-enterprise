9f55af68f208208eddcb023b53f495d16ed09b1b
Agus <Agus@HomePC>
2017-03-04 14:37:36 -0600

    Added Generic Methods

--- lma-enterprise-dao/src/main/java/org/lma/enterprise/dao/DAO.java
+++ lma-enterprise-dao/src/main/java/org/lma/enterprise/dao/DAO.java
@@ -6,6 +6,7 @@ import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.type.StandardBasicTypes;
+import org.lma.enterprise.exception.ExceptionDao;
 import org.lma.enterprise.util.Sequence;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -18,13 +19,46 @@ public class DAO<CLAZZ extends Serializable> implements DAOi<CLAZZ>{
         return sessionFactory.getCurrentSession();
     }
  
+    /**
+	 * Inserts a new VO.
+	 * 
+	 * @param vo
+	 * @return CLAZZ
+	 * @throws ExceptionDAO
+	 */
+	public CLAZZ insert(CLAZZ vo) throws ExceptionDao{
+		
+		try{
+			getSession().persist(vo);
+		}catch(Exception e){
+			throw new ExceptionDao(e.getMessage());
+		}
+		
+		return vo;
+	}
+    
+	/**
+	 * Deletes a VO.
+	 * 
+	 * @param vo
+	 * @throws ExceptionDao
+	 */
+	public void delete(CLAZZ vo) throws ExceptionDao{
+		try{
+			getSession().delete(vo);
+		}catch(Exception e){
+			throw new ExceptionDao(e.getMessage());
+		}
+	}
+	
+	/*
     public void persist(Object entity) {
         getSession().persist(entity);
     }
  
     public void delete(Object entity) {
         getSession().delete(entity);
-    }
+    }*/
     
     public Integer getNext(Sequence seq){
     	
--- lma-enterprise-dao/src/main/java/org/lma/enterprise/dao/DAOi.java
+++ lma-enterprise-dao/src/main/java/org/lma/enterprise/dao/DAOi.java
@@ -2,6 +2,24 @@ package org.lma.enterprise.dao;
 
 import java.io.Serializable;
 
+import org.lma.enterprise.exception.ExceptionDao;
+
 public interface DAOi<CLAZZ extends Serializable> {
 
+	/**
+	 * Inserts a new VO.
+	 * 
+	 * @param vo
+	 * @return CLAZZ
+	 * @throws ExceptionDAO
+	 */
+	public CLAZZ insert(CLAZZ vo) throws ExceptionDao;
+	
+	/**
+	 * Deletes a VO.
+	 * 
+	 * @param vo
+	 * @throws ExceptionDao
+	 */
+	public void delete(CLAZZ vo) throws ExceptionDao;
 }
